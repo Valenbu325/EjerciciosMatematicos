fe2581db61585c3d872ea3bb3ba3da0434e5a412
unknown <RGH-LS24-09@RGH-LS24-09.UMD.LOCAL.EDU>
2025-09-25 18:45:04 -0500

    changes app.py and controller

--- PythonExercies/app.py
+++ PythonExercies/app.py
@@ -1,7 +1,8 @@
 from view.menu import mostrar_menu, pedir_opcion
 from controller.controller import ejecutar_ejercicio
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+    while True:
         mostrar_menu()
         opcion = pedir_opcion()
         if opcion == "0":
