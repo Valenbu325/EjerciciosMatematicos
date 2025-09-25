cc926948a245cf2970c4e0e5051de32b1445d5f1
Valenbu325 <valentina.burgos-a@uniminuto.edu.co>
2025-09-25 18:22:22 -0500

    Update app.py

--- PythonExercies/app.py
+++ PythonExercies/app.py
@@ -1,8 +1,7 @@
 from view.menu import mostrar_menu, pedir_opcion
 from controller.controller import ejecutar_ejercicio
 
-if __name__ == "__main__":  # ← Solo cambiar ** por __
-    while True:
+if __name__ == "__main__": 
         mostrar_menu()
         opcion = pedir_opcion()
         if opcion == "0":
