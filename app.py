a93d3238bcba6d4ced66fe424457936171b5a700
Valenbu325 <valentina.burgos-a@uniminuto.edu.co>
2025-09-25 11:51:57 -0500

    changes

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from view.menu import menu, pedir_numero, mostrar_resultado
-from controller.controller import ejecutar_suma
+from view.menu import pedir_opcion, mostrar_menu
+from controller.controller import ejecutar_ejercicio
+
 
 if __name__ == "__main__":
     while True:
-        opcion = menu()
-        if opcion == "1":
-            n = pedir_numero()
-            resultado = ejecutar_suma(n)
-            mostrar_resultado(resultado)
-        elif opcion == "0":
+        mostrar_menu()
+        opcion = pedir_opcion()
+        if opcion == "0":
             print("Saliendo...")
             break
         else:
-            print("Opción no válida")
+            ejecutar_ejercicio(opcion)
