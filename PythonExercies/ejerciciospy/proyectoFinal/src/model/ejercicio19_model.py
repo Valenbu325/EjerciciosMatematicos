8f85b2640cc31bc8f4b976b43c264ec185b8929d
Valenbu <valentina123acosta@gmail.com>
2025-09-24 22:53:11 -0500

    Create and modify folders and files.

--- PythonExercies/ejerciciospy/proyectoFinal/src/model/ejercicio19_model.py
+++
@@ -1,9 +0,0 @@
-class Ejercicio19Model:
-    def f(self, n: int) -> float:
-        """Función recursiva de la sucesión."""
-        if n == 1:
-            return 2
-        elif n >= 2:
-            return n + 1 / self.f(n - 1)
-        else:
-            raise ValueError("n debe ser un entero positivo.")
--- PythonExercies/ejerciciospy/proyectoFinal/src/model/ejercicio9_model.py
+++
@@ -1,6 +0,0 @@
-def log_entero(n, base):
-    resultado = 0
-    while n >= base:
-        n //= base
-        resultado += 1
-    return resultado
